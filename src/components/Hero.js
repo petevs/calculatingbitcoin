ddd4c1e162e90f65a1cbba0cd0b6e4c39268b2ce
Pete VS <pete_vs88@me.com>
2021-09-08 11:43:52 -0600

    created basic hero component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,8 +4,8 @@ import styled from 'styled-components'
 const Hero = ({ title, subtitle }) => {
     return (
         <Wrapper>
-            <h2>{title}</h2>
-            <h4>{subtitle}</h4>
+            <Headline>{title}</Headline>
+            <h2>{subtitle}</h2>
         </Wrapper>
     )
 }
@@ -14,7 +14,13 @@ export default Hero
 
 const Wrapper = styled.div`
     display: grid;
+    min-height: 300px;
     grid-template-columns: 1fr;
     justify-content: center;
+    align-content: center;
     text-align: center;
+`
+
+const Headline = styled.h1`
+    font-size: 3.2rem;
 `
