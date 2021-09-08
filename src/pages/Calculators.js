c7a2f8accdc6e31580f176957035d02337bfacf8
Pete VS <pete_vs88@me.com>
2021-09-08 12:12:07 -0600

    added router

---
+++ src/pages/Calculators.js
@@ -0,0 +1,15 @@
+import Hero from 'components/Hero'
+import React from 'react'
+
+const Calculators = () => {
+    return (
+        <div>
+            <Hero
+                title='Calculators'
+                subtitle='list of calculators'
+            />
+        </div>
+    )
+}
+
+export default Calculators
