bf0350a4145ec5501acd3d690a56c5bba364a038
Pete VS <pete_vs88@me.com>
2021-09-10 11:11:20 -0600

    added route for dca

---
+++ src/calculators/dca/DollarCostAverage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DollarCostAverage = () => {
+    return (
+        <div>
+            I am the dollar cost average
+        </div>
+    )
+}
+
+export default DollarCostAverage
