a9e53cb799a6a29b9f3b8645fa9cd90afae851a1
Pete VS <pete_vs88@me.com>
2021-09-13 13:16:51 -0600

    running bal workign

--- src/calculators/dca/DollarCostAverage.js
+++ src/calculators/dca/DollarCostAverage.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import axios from 'axios'
 import Row from 'components/Row'
+import { TextField } from '@material-ui/core'
 
 const DollarCostAverage = () => {
 
@@ -22,6 +23,14 @@ const DollarCostAverage = () => {
 
     const [daysBtwn, setDaysBtwn] = useState(null)
     const [prices, setPrices] = useState([])
+    const [inputs, setInputs] = useState({
+        purchaseAmount: '5'
+    })
+
+
+    const handleChange = (e) => {
+        console.log(e.target.value)
+    }
 
     const handleDateChange = (e) => {
         setDaysBtwn(getTimeBetweenDates(e.target.value))
@@ -35,21 +44,39 @@ const DollarCostAverage = () => {
                 const data = res.data.prices
                 const historicalData = []
 
+                let runningBal = 0
+
                 data.map(item => {
                     let friendlyDate = convertDate(item[0])
+
+                    const price = Math.round(item[1])
+                    const bitcoinAdded = Number((5 / Math.round(item[1])).toFixed(8))
+
+                    runningBal = runningBal + bitcoinAdded
+
+                    const value = Number((price * runningBal).toFixed(2))
+
                     historicalData.push({
                         date: friendlyDate,
-                        price: Math.round(item[1]),
-                        bitcoinAdded: (5 / Math.round(item[1])).toFixed(8)
+                        price: price,
+                        bitcoinAdded: bitcoinAdded,
+                        bal: runningBal.toFixed(8),
+                        value: value
                     })
                 })
+
                 setPrices(historicalData)
+
             })
     }
 
     return (
         <div>
             {/* {convertDate('2018-01-01')} */}
+            <TextField 
+                label='Purchase Amount'
+                onChange={handleChange}
+            />
             <label for='start'>Start Date:</label>
             <input id='start' type='date' onChange={handleDateChange} />
             <button onClick={getValues}>Calculate</button>
