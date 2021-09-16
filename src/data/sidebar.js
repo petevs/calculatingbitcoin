23e386b30c0963b7cc7e702fd3365e389cd65605
Pete VS <pete_vs88@me.com>
2021-09-16 15:04:56 -0600

    added more placeholders

--- src/data/sidebar.js
+++ src/data/sidebar.js
@@ -3,6 +3,7 @@ import QueryStatsIcon from '@mui/icons-material/QueryStats';
 import PersonIcon from '@mui/icons-material/Person';
 import SettingsIcon from '@mui/icons-material/Settings';
 import CalculateIcon from '@mui/icons-material/Calculate';
+import ListAltIcon from '@mui/icons-material/ListAlt';
 
 
 export const portfolio = {
@@ -31,7 +32,58 @@ export const menuList = [
             {
                 title: 'Dollar Cost Average',
                 path: '/calculators/dca'
-            }
+            },
+            {
+                title: 'Buy The Dip',
+                path: ''
+            },
+            {
+                title: 'DCA vs Buy The Dip',
+                path: '/calculators/dca'
+            },
+            {
+                title: 'Retire on Bitcoin',
+                path: ''
+            },
+            {
+                title: 'Speculative Attack',
+                path: ''
+            },
+            {
+                title: 'Opportunity Cost',
+                path: ''
+            },
+            {
+                title: 'Scenario Planning',
+                path: ''
+            },
+            {
+                title: 'Tax Liability',
+                path: ''
+            },
+    ]
+    },
+    {
+        title: 'Resources',
+        icon: <ListAltIcon />,
+        subMenu: [
+            {
+                title: 'Articles',
+                path: ''
+            },
+            {
+                title: 'People to Follow',
+                path: ''
+            },
+            {
+                title: 'Cool Companies',
+                path: ''
+            },
+            {
+                title: 'Hardware Wallets',
+                path: ''
+            },
+
     ]
     },
     {
