d6201d8638f9d0272320a5a474649406fd683d1c
Pete VS <pete_vs88@me.com>
2021-09-27 12:47:01 -0600

    changed usercontext

--- src/state/contexts/UserContext.js
+++ src/state/contexts/UserContext.js
@@ -40,11 +40,11 @@ const UserProvider = ({children}) => {
 
     useEffect(() => {
         axios.get(`https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=${settings.currency}&days=3650&interval=daily`)
-        .then((res) => {
-          const data = res.data.prices;
-          calculatorsDispatch(updateDcaHistoricalData(data))
-          portfolioDispatch(updatePriceHistory(data))
-        })
+            .then((res) => {
+            const data = res.data.prices;
+            calculatorsDispatch(updateDcaHistoricalData(data))
+            portfolioDispatch(updatePriceHistory(data))
+            })
     },[settings.currency])
 
 
